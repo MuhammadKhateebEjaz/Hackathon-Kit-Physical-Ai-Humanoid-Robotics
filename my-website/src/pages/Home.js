a13dd9536817abaef52fd4e4a8efdca4a3b5cc36
Muhammad Khateeb Ejaz <your_email@example.com>
2025-12-21 04:41:05 +0500

    Initial commit: Upload Hackathon Kit Project

---
+++ my-website/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import clsx from 'clsx';
+import styles from './index.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {/* Hero Section */}
+      <div className={styles.hero}>
+        <div className={styles.heroText}>
+          <center>
+            <h1>Fundamentals of Physical AI & Humanoid Robotics</h1>
+            <p>A Beginner's Guide to Intelligent Robots</p>
+          </center>
+          <div className={styles.buttons}>
+            <Link className={clsx('button button--primary button--lg')} to="/docs/intro">
+              Get Started
+            </Link>
+            <Link className={clsx('button button--secondary button--lg')} to="/docs/intro">
+              View Chapters
+            </Link>
+          </div>
+        </div>
+        <div className={styles.heroImage}>
+          <img src="/img/Robot-cover.jpg" alt="Humanoid Robot" />
+        </div>
+      </div>
+    </main>
+  );
+}
